e888653c15974a481aca857704f4b0016851f06c
JaleNemo <109641106+JaleNemo@users.noreply.github.com>
2024-11-24 07:37:02 +0000

    edit

--- calculator.py
+++ calculator.py
@@ -1,25 +1,46 @@
 class Calculator:
+    
     def add(self, a, b):
+        if type(a) != int or type(b) != int:
+            return "Please enter only integer"
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        if type(a) != int or type(b) != int:
+            return "Please enter only integer"
+        return a - b
 
     def multiply(self, a, b):
+        if type(a) != int or type(b) != int:
+            return "Please enter only integer"
         result = 0
-        for i in range(b+1):
+        for i in range(abs(b)):
             result = self.add(result, a)
+        if (b < 0):
+            return -result
         return result
 
     def divide(self, a, b):
+        if type(a) != int or type(b) != int:
+            return "Please enter only integer"
+        if b == 0:
+            return "Divided by zero"
+        
+        negative = (a < 0) != (b < 0)  
+        a, b = abs(a), abs(b)
+        
         result = 0
-        while a > b:
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
+        
+        if negative:
+            result = -result
+        
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,33 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
-    # Add the following test methods to the TestCalculator class:
+    def test_add_2(self):
+        self.assertEqual(self.calc.add(0, 1), 1)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+
+    def test_subtract_2(self):
+        self.assertEqual(self.calc.subtract(0, 1), -1)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 1), 2)
+
+    def test_multiply_2(self):
+        self.assertEqual(self.calc.multiply(0, 11), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4,2), 2)
+
+    def test_divide_2(self):
+        self.assertEqual(self.calc.divide(0, 2), 0)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(4,2), 0)
+
+    def test_modulo_2(self):
+        self.assertEqual(self.calc.modulo(5,2), 1)
+        
 
 if __name__ == '__main__':
     unittest.main()
